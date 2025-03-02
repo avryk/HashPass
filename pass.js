fe1febc697202092ec2d0e0181a1e0d0075f4004
Gonçalo Lourenço a22106337 <128821979+avryk@users.noreply.github.com>
2025-03-02 23:29:35 +0000

    aaa

--- pass.js
+++ pass.js
@@ -1,1 +1,9 @@
+const{createHash} = require('crypto');
 
+function hash(input){
+    return createHash('sha256').update(input).digest('hex');
+}
+
+let password = 'abcde256'
+const hash1 = hash(password);
+console.log(hash1);
